edcb3bd520a71b4a355db222bd529bb9ebfe8bd6
aryan-67 <hello.rn456@gmail.com>
2026-03-13 21:17:13 +0530

    Add config.js

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+  collegeName: 'ICOE College',
+  collegeShortName: 'ICOE',
+  collegeLogo: null,
+  emailSuffix: null,
+  appName: 'CanteenBite',
+  appTagline: 'Smart Canteen Ordering',
+  welcomeBonus: 400,
+};
+
+export default config;
